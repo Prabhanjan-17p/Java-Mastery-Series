a92ed00db87a486277effed4efd197c533c1c466
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-02-06 00:13:59 +0530

    Update

---
+++ Kanha.java
@@ -0,0 +1,11 @@
+public class Kanha {
+
+
+    public static void main(String[] args) {
+        int x= 10;
+        int y =10;
+        int sum;
+        sum = x+y;
+        System.out.println("the sum of the 2 number is = "+sum);
+    }
+}
