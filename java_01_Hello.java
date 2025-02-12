85fabb8498fcae8b9a876cafc3583b21708032bc
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-02-13 01:31:41 +0530

    first programm or print hello world

---
+++ java_01_Hello.java
@@ -0,0 +1,18 @@
+//package JAVA;
+
+public class java_01_Hello {
+    public static void main(String[] args) {
+                System.out.println("hello world");
+
+//                System.out.println( "Hello" + args[0] );
+
+//        System.out.println(Byte.MAX_VALUE);
+//        System.out.println(Byte.MIN_VALUE);
+
+
+
+        System.out.println(4 * 1000);
+
+    }
+
+}
