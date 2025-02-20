6ee17423f85c328fa3f6b90421e20ad3bb7265c7
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-02-21 00:33:57 +0530

    What is boolean in java and how it is work how to implement

---
+++ java_04_Bool.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class java_04_Bool {
+    public static void main(String[] args) {
+        System.out.println("Check The Value is True Or False ");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your value :-");
+        boolean Val = sc.hasNextInt();
+        System.out.println(Val);
+    }
+    
+}
