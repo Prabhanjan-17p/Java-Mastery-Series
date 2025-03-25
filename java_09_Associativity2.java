0fc738dbab44f90fae6502b8016bd1d8aa4fc03d
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-03-25 22:31:03 +0530

    Associativity in java

---
+++ java_09_Associativity2.java
@@ -0,0 +1,18 @@
+public class java_09_Associativity2 {
+    public static void main(String []args)
+    {
+        System.out.println("it is example of associativity operation:");
+        int a=5;
+        int b=2;
+        int c=3;
+        int k=b*b-(4*a*c)/(2*a);
+              //2*2-(4*5*3)/(2*5)
+//             2*2-(60)/(10)
+//             2*2-6
+//             4-6
+//            = -2
+        System.out.println("the ans will be:"+k);
+
+    }
+    
+}
